0edcc7361b4e94bd416ac909866160109b3f997a
ninjaboynaru <thiagoCord755@gmail.com>
2018-05-02 17:13:24 +0000

    httpError builder documentation

--- app/services/httpError.js
+++ app/services/httpError.js
@@ -1,4 +1,14 @@
-
+/**
+* Module to build HTTP error response objects in a uniform manner
+*
+* Response from building an error is an object with 2 properties
+* {object} payload - Response to send to client
+* {object} internal - Information for internal applicaiton usage
+*
+* To add additional HTTP error types, add a new function to the "builders" object
+* The function should return the result of callng the "builders.general" function in order to
+* ensure all errors are unifrom in structure
+*/
 
 const builders = {};
 
@@ -59,6 +69,16 @@ builders.notFound = function notFound({ message = 'The requested resource could 
 	});
 };
 
+/**
+* Build HTTP error objects in a uniform and consistent manner
+* @property {function} set - Set the value of a setting. First param is {string} setting name, and second
+* is the settings' value
+*
+* @property {function} build - Builds an error object. First parameter is {string} error-builder function name
+* Second param is an object to pass the the builder function.
+* If an error-builder function with the specified name does not exist an error will be thrown, unless
+* the "throwOnNotFound" setting is set to false
+*/
 const httpError = {
 	settings: {
 		throwOnNotFound: true,
